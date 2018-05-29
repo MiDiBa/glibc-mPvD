58cb2cbfff2a410a4f790330e0c70a8cfcf20e1e
Di Bartolomeo Michael <m.dibartolomeo@student.ulg.ac.be>
2018-05-29 15:35:31 +0200

    New results

--- tests/performances.sh
+++ tests/performances.sh
@@ -8,7 +8,7 @@ if [ -f perf_PvD.txt ]; then
     rm perf_PvD.txt
 fi
 
-for value in {1..100}
+for value in {1..1000}
 do
     sudo ./run_tests.sh dns_resolv_perf 0 www.google.fr >> perf_noPvD.txt
     sudo ./run_tests.sh dns_resolv_perf 1 www.google.fr >> perf_PvD.txt
